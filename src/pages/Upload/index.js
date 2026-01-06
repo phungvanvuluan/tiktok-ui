8ca1d2e4cf1066119314a85cbcdc7155ecd9d05c
luan <luanphung1357@gmail.com>
2026-01-06 16:35:26 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,6 @@
+
+function UpLoad() {
+    return <h2>UpLoad </h2>;
+}
+
+export default UpLoad;
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+
+//layoyt
+import HeaderOnly from '~/components/Layout/HeaderOnly'
+
+
+//pages
+import Folowing from '~/pages/Folowing'
+import Home from '~/pages/Home'
+import Profile from '~/pages/Profile'
+import UpLoad from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+
+
+//public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Folowing },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: UpLoad, layout : HeaderOnly },
+    { path: '/search', component: Search, layout : null },
+
+]
+
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
