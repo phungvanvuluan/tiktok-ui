a9515a7a2f63a63f6a49fb30808780cce27d3a4f
luan <luanphung1357@gmail.com>
2026-02-02 10:12:23 +0700

    Xây dựng UI phần sidebar #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,11 +9,13 @@ import Home from '~/pages/Home';
 import Profile from '~/pages/Profile';
 import UpLoad from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 //public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Folowing },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: UpLoad, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
